7f00fa98a1c345c114e41eaf10c4ab7f2c8a2fb3
su-kun1899 <higedrum.coz@gmail.com>
2016-12-31 21:13:45 +0900

    Modify controller

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,44 @@
 class TopController < ApplicationController
+  layout 'top'
+
+  #TODO ファイル名と保持期間を定数化したい
+
+  def initialize
+    super
+    begin
+      @messages = JSON.parse(File.read('messages.txt'))
+    rescue
+      @messages = Hash.new
+    end
+    @messages.each do |post_time, message|
+      if Time.now.to_i - post_time.to_i > 24*60*60
+        @messages.delete(post_time)
+      end
+    end
+
+    File.write('messages.txt', @messages.to_json)
+  end
+
   def index
+    if request.post?
+      message = Message.new(message: params['message'], name: params['name'], mail: params['mail'])
+      @messages[Time.now.to_i] = message
+      File.write('messages.txt', @messages.to_json)
+
+      @messages = JSON.parse(@messages.to_json)
+    end
+
+  end
+end
+
+class Message
+  attr_accessor :name
+  attr_accessor :mail
+  attr_accessor :message
+
+  def initialize(name: name, mail: mail, message: message)
+    self.name = name
+    self.mail = mail
+    self.message = message
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'top/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top/index', to: 'top#index'
+  post 'top/index', to: 'top#index'
+  get 'top/index'
+  post 'top/index'
 end
